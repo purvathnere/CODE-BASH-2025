29d2d5fb724cb876a1ec7c11bf47ebbe5ba38479
PURVA ATHNERE <93878803+purvathnere@users.noreply.github.com>
2024-08-09 01:11:30 +0530

    Update ReverseLinkedList.cpp

--- Linked List/ReverseLinkedList.cpp
+++ Linked List/ReverseLinkedList.cpp
@@ -1,3 +1,5 @@
+
+//iterative approach
 class Solution {
   public:
     // Function to reverse a linked list.
@@ -13,5 +15,26 @@ class Solution {
             curr=next;
         }
         return prev;
+    }
+};
+//recurive approcah
+class Solution {
+private:
+    void reverse(ListNode* &head,ListNode* curr, ListNode* prev) {
+        if (curr == NULL) {
+            head = prev;
+            return;
+        }
+       ListNode* forward = curr->next;
+        reverse(head, forward, curr);
+        curr->next = prev;
+    }
+
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        reverse(head, curr, prev);
+        return head;
     }
 };
