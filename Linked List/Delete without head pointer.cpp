9a478c46d879d0eb50b9b7684b3611f8223500e9
PURVA ATHNERE <93878803+purvathnere@users.noreply.github.com>
2024-08-16 00:53:06 +0530

    Update Delete without head pointer.cpp

--- Linked List/Delete without head pointer.cpp
+++ Linked List/Delete without head pointer.cpp
@@ -5,12 +5,12 @@ class Solution
     void deleteNode(Node *del_node)
     {
         //node 1 2 3 4 5
-       Node* prev=del_node; //3
-       Node* curr=del_node->next; //4
+       Node* prev=del_node; //3->4 (3 memery free ho gaya: linked list se chala gya)
+       Node* curr=del_node->next; //4 ( yeh del kardege)
        Node* next=del_node->next->next; //5
        //copy data
        prev->data=curr->data;
-       //link
+       //link kar diya
        prev->next=next;
        
        delete curr;
